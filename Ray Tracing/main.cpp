d5427a6f69c0f43d6f87e9429a0c03ae31aa2726
adamsvestka <svestka.adam1@gmail.com>
2020-04-13 00:29:58 +0200

    Initial Commit

---
+++ Ray Tracing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ray Tracing
+//
+//  Created by Adam Svestka on 4/13/20.
+//  Copyright © 2020 Adam Svestka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
